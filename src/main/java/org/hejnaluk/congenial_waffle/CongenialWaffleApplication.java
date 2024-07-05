519d2c5625a9d54683d28945360d2d62600935c2
Lukáš Hejnal <kejhy93@gmail.com>
2024-07-05 20:01:47 +0000

    Initial commit

---
+++ src/main/java/org/hejnaluk/congenial_waffle/CongenialWaffleApplication.java
@@ -0,0 +1,13 @@
+package org.hejnaluk.congenial_waffle;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CongenialWaffleApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CongenialWaffleApplication.class, args);
+	}
+
+}
---
+++ src/test/java/org/hejnaluk/congenial_waffle/CongenialWaffleApplicationTests.java
@@ -0,0 +1,13 @@
+package org.hejnaluk.congenial_waffle;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CongenialWaffleApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
